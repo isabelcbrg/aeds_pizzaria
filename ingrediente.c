0a6a15728d881bfe01c0038f33fa4e9e2ceef010
DaviManoelB <davi.bernardes@sga.pucminas.br>
2024-12-09 07:28:11 -0300

    Update ingrediente.c

--- ingrediente.c
+++ ingrediente.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "ingrediente.h"
+#include "header/ingrediente.h"
 
 Ingrediente *ingredientes = NULL;
 int numIngredientes = 0;
